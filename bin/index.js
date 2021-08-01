3e2c92262c8f39ba9d2faba7ad1fb3360be7556b
Ignacio Martinez <ignacio@teamthunderfoot.com>
2021-08-01 17:21:54 +0200

    update coinbase pro

--- bin/index.js
+++ bin/index.js
@@ -10,38 +10,15 @@ var auth = params.auth;
 const authedClient = new CoinbasePro.AuthenticatedClient(auth.apiKey, auth.apiSecret, auth.passphrase, auth.apiURI);
 
 var minutes = 2, the_interval = minutes * 60 * 1000;
-console.log("VALOR MAS ALTO DE VENTA:", historyGetHighestSellPrice());
-console.log("VALOR MAS BAJO DE COMPRA:", historyGetLowestBuyPrice());
 
-function getEUR(callback){
-    return authedClient.getAccounts((error, response, data) => {
-        if (error) {
-            console.log(error);
-        } else {
-            callback(data.filter((el) => {
-                return el.currency == 'EUR'
-            }));
-        }
-    });
-}
-
-function getSUSHI() {
-    return authedClient.getAccounts((error, response, data) => {
-        if (error) {
-            console.log(error);
-        } else {
-            return data.filter((el) => {
-                return el.currency == 'SUSHI'
-            });
-        }
-    });
-}
-
+var curAssetIndex = 2;
 setInterval(async function() {
-    var raw_market = fs.readFileSync('bin/market.json');
-    var market = JSON.parse(raw_market);
+    var asset = params.assets[curAssetIndex];
+    console.log(`Analizando ${asset}...`);
+    const raw_market = fs.readFileSync('bin/' + asset + '.market.json');
+    const market = JSON.parse(raw_market);
 
-    var raw_trans = fs.readFileSync('bin/transactions.json');
+    var raw_trans = fs.readFileSync('bin/' + asset + '.transactions.json');
     var trans = JSON.parse(raw_trans);
 
     const history = await authedClient.getProductHistoricRates(market.crypto, { granularity: 60 });
@@ -55,14 +32,14 @@ setInterval(async function() {
                 sell(data, market, trans);
             } else if(market.last_operation == "SELL") {
                 // we buy
-                buy(data, market, trans);
+                buy(asset, data, market, trans);
             }
             
         }
     });
 }, the_interval);
 
-async function buy(data, market, trans) {
+async function buy(asset, data, market, trans) {
     let currentPrice = parseFloat(data.price);
     let lastPrice = parseFloat(market.last_price);
     let allocation = parseFloat(market.allocation);
@@ -95,14 +72,14 @@ async function buy(data, market, trans) {
 
                 market.last_price = currentPrice;
                 market.last_operation = "BUY";
-                fs.writeFileSync('bin/market.json', JSON.stringify(market));
+                fs.writeFileSync('bin/' + asset + '.market.json', JSON.stringify(market));
             }
         });
     }
     console.log("---");
 }
 
-async function sell(data, market, trans) {
+async function sell(asset, data, market, trans) {
     let currentPrice = parseFloat(data.price);
     let lastPrice = parseFloat(market.last_price);
     let allocation = parseFloat(market.allocation);
@@ -111,7 +88,7 @@ async function sell(data, market, trans) {
     const sellPrice = lastPrice * (1 + spread);
 
     const balances = await authedClient.getAccounts();
-    const assetBalance = parseFloat(balances.filter(el => { return el.currency == 'ADA' })[0].balance);
+    const assetBalance = parseFloat(balances.filter(el => { return el.currency == asset })[0].balance);
     const sellVolume = assetBalance * allocation;
     
     console.log("VENTA", {"Precio de venta": sellPrice, "Precio Actual": currentPrice});
@@ -122,7 +99,7 @@ async function sell(data, market, trans) {
         const params = {
             side: 'sell',
             size: assetBalance,
-            product_id: market.crypto, // SUSHI-EUR
+            product_id: market.crypto,
             type : 'market'
         };
         authedClient.placeOrder(params, (error, response, order) => {
@@ -132,11 +109,11 @@ async function sell(data, market, trans) {
                 order['price'] = currentPrice;
                 console.log("VENTA HECHA", order);
                 trans.push(order);
-                fs.writeFileSync('bin/transactions.json', JSON.stringify(trans));
+                fs.writeFileSync('bin/' + asset + '.transactions.json', JSON.stringify(trans));
 
                 market.last_price = currentPrice;
                 market.last_operation = "SELL";
-                fs.writeFileSync('bin/market.json', JSON.stringify(market));
+                fs.writeFileSync('bin/' + asset + '.market.json', JSON.stringify(market));
             }
         });
     }
